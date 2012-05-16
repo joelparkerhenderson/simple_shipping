5eb3eb0dd5d438373afa4635bc9461aae6e40e90
Zachary Belzer <zach@tmxcredit.com>
2012-05-16 12:00:23 -0500

    Clean up some shared properties. Establish HVR for cert.
    Task 3749

--- lib/simple_shipping/ups/response.rb
+++ lib/simple_shipping/ups/response.rb
@@ -8,6 +8,10 @@ module SimpleShipping::Ups
       value_of(:shipment_results, :shipment_identification_number)
     end
 
+    def tracking_number
+      value_of(:shipment_results, :package_results, :tracking_number)
+    end
+
     # Fetches the value of an XML attribute at the path specified as an array
     # of node names but appends the implicit namespace on to the front of the
     # path
@@ -21,5 +25,25 @@ module SimpleShipping::Ups
       self.class.name.split('::').last.underscore.to_sym
     end
     private :name_token
+  end
+
+  module SharedResponseAttributes
+    # Get the label as base64 encoded data
+    #   response.label_image_base64 # => "odGqk/KmgLaawV..."
+    # This can be used directly in an HTML image tag with
+    #   src="data:image/gif;base64,..."
+    def label_image_base64
+      value_of(:shipment_results, :package_results, :shipping_label, :graphic_image)
+    end
+
+    def label_html
+      value = value_of(:shipment_results, :package_results, :shipping_label, :html_image)
+      Base64.decode64(value) if value
+    end
+
+    def receipt_html
+      value = value_of(:shipment_results, :control_log_receipt, :graphic_image)
+      Base64.decode64(value) if value
+    end
   end
 end
--- lib/simple_shipping/ups/response/shipment_response.rb
+++ lib/simple_shipping/ups/response/shipment_response.rb
@@ -1,20 +1,6 @@
 # A wrapper for UPS ShipmentResponse
 module SimpleShipping::Ups
   class ShipmentResponse < Response
-    # Get the label as abstract64 encoded data
-    #   response.label_image_base64 # => "odGqk/KmgLaawV..."
-    # This can be used directly in an HTML image tag with
-    #   src="data:image/gif;base64,..."
-    def label_image_base64
-      value_of(:shipment_results, :package_results, :shipping_label, :graphic_image)
-    end
-
-    def label_html
-      Base64.decode64 value_of(:shipment_results, :package_results, :shipping_label, :html_image)
-    end
-
-    def receipt_html
-      Base64.decode64 value_of(:shipment_results, :control_log_receipt, :graphic_image)
-    end
+    include SharedResponseAttributes
   end
 end
